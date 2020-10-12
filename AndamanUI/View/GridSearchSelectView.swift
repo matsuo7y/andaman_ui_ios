6fccdc4070c12a5aa140b129213d1bae83bc69dd
Yuki Matsuo <yukimaterrace@gmail.com>
2020-10-12 10:14:58 +0900

    improve foreach

--- AndamanUI/View/GridSearchSelectView.swift
+++ AndamanUI/View/GridSearchSelectView.swift
@@ -8,43 +8,35 @@
 import SwiftUI
 
 struct GridSearchSelectView: View {
-    @State var pairSelected = 0
-    @State var timezoneSelected = 0
-    @State var directionSelected = 0
-    @State var algorithmSelected = 0
+    @State var pairSelected = TradePair.GbpUsd
+    @State var timezoneSelected = Timezone.tokyoAM
+    @State var directionSelected = TradeDirection.long
+    @State var algorithmSelected = TradeAlgorithm.reaction
     
     var body: some View {
         NavigationView {
             Form {
                 Picker(selection: $pairSelected, label: Text("Pair")) {
-                    ForEach(0..<TradePair.allCases.count) {
-                        Text(TradePair.allCases[$0].display)
-                    }
+                    ForEach(TradePair.allCases, id: \.self) { Text($0.display) }
                 }
                 
                 Picker(selection: $timezoneSelected, label: Text("Timezone")) {
-                    ForEach(0..<Timezone.allCases.count) {
-                        Text(Timezone.allCases[$0].display)
-                    }
+                    ForEach(Timezone.allCases, id: \.self) { Text($0.display) }
                 }
                 
                 Picker(selection: $directionSelected, label: Text("Direction")) {
-                    ForEach(0..<TradeDirection.allCases.count) {
-                        Text(TradeDirection.allCases[$0].display)
-                    }
+                    ForEach(TradeDirection.allCases, id: \.self) { Text($0.display) }
                 }
                 
                 Picker(selection: $algorithmSelected, label: Text("Algorithm")) {
-                    ForEach(0..<TradeAlgorithm.allCases.count) {
-                        Text(TradeAlgorithm.allCases[$0].display)
-                    }
+                    ForEach(TradeAlgorithm.allCases, id: \.self) { Text($0.display) }
                 }
                 
                 NavigationLink(destination: GridSearchGrainView(
-                                pair: TradePair.allCases[pairSelected],
-                                timezone: Timezone.allCases[timezoneSelected],
-                                direction: TradeDirection.allCases[directionSelected],
-                                algorithm: TradeAlgorithm.allCases[algorithmSelected])
+                                pair: pairSelected,
+                                timezone: timezoneSelected,
+                                direction: directionSelected,
+                                algorithm: algorithmSelected)
                 ) {
                     Text("Select")
                 }
